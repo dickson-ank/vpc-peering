8be9052256e0f6e3c5859d3ecc72a51bb2c21a94
dickson-ank <dickson.ankamah8@gmail.com>
2025-10-15 23:30:34 +0000

    Modify testing

--- components/pagesections/step-6.tsx
+++ components/pagesections/step-6.tsx
@@ -44,18 +44,13 @@ export function Step6({setSelectedImage}: Step6Props){
                 <span className="text-muted-foreground mb-1 block">ssh -i YOUR-KEY-PAIR.pem ec2-user@ENI-PRIVATE-IP</span>
               </span> <br />
             </Paragraph>
-            <ImageContainer src="./ssh-into-instance.png" 
-            alt="SSH into the App server instance from the Bastion Host" selectedImage={setSelectedImage}/>
 
             <Paragraph>
                We have confirmed the whole setup is working as expected! <br />
             </Paragraph>
 
             <Paragraph>
-              You can run further tests on your own to verify if the security groups are working as intended <br />
-              For example try to send http requests to the site on a different port (eg, 54322) to see if it will respond or not <br />
-              You can also try to ssh into the instances directly using their private IPs to see if it will work or not <br />
-            </Paragraph>
+              You can run further tests on your own to verify if the security groups are working as intended <br />            </Paragraph>
             <h4 className="text-xl sm:text-xl md:text-2xl lg:text-2xl font-bold text-foreground mb-4 text-balance">
                Congratulations!</h4>
             That's all about it, we've been able to successfully complete the design
